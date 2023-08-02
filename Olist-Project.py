63717ea9394328e0907a3d7e72fcf1b81473491e
Joseph Sauerland <32148757+jsauerland@users.noreply.github.com>
2023-08-02 17:13:14 -0400

    added service account keys

--- Olist-Project.py
+++ Olist-Project.py
@@ -1,12 +1,30 @@
+#Import libraries needed
+from google.cloud import bigquery
+from google.cloud.exceptions import NotFound
+from google.oauth2 import service_account
 import kaggle
+from kaggle.api.kaggle_api_extended import KaggleApi
+import os
 import pandas as pd
 import pandas_gbq
-import os
-from google.cloud import bigquery
+import zipfile
+
+# Configure Kaggle API
 api = kaggle.api
-datasets = kaggle.api.dataset_list(search="covid")
+
+# Set Google Application Credentials
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'BQ_KEY.json' # this is your account details JSON file
+
+
+# Create a BigQuery client instance (bigquery_client)
+
+client = bigquery.Client() # Create a BigQuery client instance (bigquery_client)
+credentials = service_account.Credentials.from_service_account_file(
+    'BQ_KEY.json',
+)
 
 # kaggle.api.dataset_download_files('olistbr/brazilian-ecommerce',  unzip=True) # This is commented out after it is run because re-running it will re-pull the data from Kaggle using bandwith and CPU resources unnecessarily
+
 
 
 csv_files = [
